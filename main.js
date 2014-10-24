aba66771eac7128c9e1303c86a90d9ab0ade3a47
Benjamin Cabé <benjamin@eclipse.org>
2014-10-25 01:20:27 +0200

    hard code om2m and wakaama repo fix default branch for kura

--- main.js
+++ main.js
@@ -24,41 +24,41 @@ var PROJECTS = [
 ]
 
 var DATES = [
-"2012-01-01",
-"2012-02-01",
-"2012-03-01",
-"2012-04-01",
-"2012-05-01",
-"2012-06-01",
-"2012-07-01",
-"2012-08-01",
-"2012-09-01",
-"2012-10-01",
-"2012-11-01",
-"2012-12-01",
-"2013-01-01",
-"2013-02-01",
-"2013-03-01",
-"2013-04-01",
-"2013-05-01",
-"2013-06-01",
-"2013-07-01",
-"2013-08-01",
-"2013-09-01",
-"2013-10-01",
-"2013-11-01",
-"2013-12-01",
-"2014-01-01",
-"2014-02-01",
-"2014-03-01",
-"2014-04-01",
-"2014-05-01",
-"2014-06-01",
-"2014-07-01",
-"2014-08-01",
-"2014-09-01",
-"2014-10-01",
-"2014-11-01",
+	"2012-01-01",
+	"2012-02-01",
+	"2012-03-01",
+	"2012-04-01",
+	"2012-05-01",
+	"2012-06-01",
+	"2012-07-01",
+	"2012-08-01",
+	"2012-09-01",
+	"2012-10-01",
+	"2012-11-01",
+	"2012-12-01",
+	"2013-01-01",
+	"2013-02-01",
+	"2013-03-01",
+	"2013-04-01",
+	"2013-05-01",
+	"2013-06-01",
+	"2013-07-01",
+	"2013-08-01",
+	"2013-09-01",
+	"2013-10-01",
+	"2013-11-01",
+	"2013-12-01",
+	"2014-01-01",
+	"2014-02-01",
+	"2014-03-01",
+	"2014-04-01",
+	"2014-05-01",
+	"2014-06-01",
+	"2014-07-01",
+	"2014-08-01",
+	"2014-09-01",
+	"2014-10-01",
+	"2014-11-01",
 ]
 
 
@@ -69,6 +69,8 @@ function computeRepositories() {
 	for (var i in PROJECTS) {
 		var p = PROJECTS[i];
 		repositories[p] = []
+
+		console.log('Requesting info for ' + p);
 
 		request({
 			url: 'https://projects.eclipse.org/json/project/' + p,
@@ -95,33 +97,46 @@ function computeRepositories() {
 	}
 }
 
+
 function gitclone_callback(project, url, path) {
 	return function() {
 		console.log('[' + project + '] Clone of ' + url + ' in ' + path + ' ... done');
 
 		cloc(project, path, DATES, 0);
 	}
-
 }
+
+// hard code OM2M analysis
+exec('git clone git://git.eclipse.org/gitroot/om2m/org.eclipse.om2m.git /tmp/repos/999', 
+	gitclone_callback('iot.om2m', 'git://git.eclipse.org/gitroot/om2m/org.eclipse.om2m.git', '/tmp/repos/999'));
+
+// hard code Wakaama analysis
+exec('git clone https://github.com/eclipse/wakaama /tmp/repos/9999', 
+	gitclone_callback('iot.wakaama', 'https://github.com/eclipse/wakaama', '/tmp/repos/9999'));
+
 
 
 function cloc(project, path, dates, index) {
 	if (index >= dates.length) return;
 	var date = dates[index];
-	exec('GIT_DIR=' + path + '/.git git rev-list master -n 1 --first-parent --before=' + date, function(error, stdout, stderr) {
+	var branch = 'master';
+	if (project == 'iot.kura')
+		branch = 'develop';
+
+	console.log('GIT_DIR=' + path + '/.git git rev-list ' + branch + ' -n 1 --first-parent --before=' + date)
+	exec('GIT_DIR=' + path + '/.git git rev-list ' + branch + ' -n 1 --first-parent --before=' + date, function(error, stdout, stderr) {
 		console.log(path + ' - ' + date + ': ' + stdout);
 
 		if (stdout) {
 			console.log('$ git --git-dir=' + path + '/.git --work-tree=' + path + ' checkout ' + stdout);
 			exec('git --git-dir=' + path + '/.git --work-tree=' + path + ' checkout ' + stdout, function(error, stdout, stderr) {
 				if (!error) {
-					var csv_filepath = path + '/../' + project + path.replace('/tmp/repos/', ',') + ',' + date + '.csv' ;
+					var csv_filepath = path + '/../' + project + path.replace('/tmp/repos/', ',') + ',' + date + '.csv';
 					console.log('$ cloc ' + path + ' --csv --report-file=' + csv_filepath);
 					exec('cloc ' + path + ' --csv --report-file=' + csv_filepath, function(error, stdout, stderr) {
 						if (!error) {
 							cloc(project, path, dates, index + 1);
-						}
-						else {
+						} else {
 							console.log(error);
 						}
 					});
