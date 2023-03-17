294f2d2aff215294177aa0e5f968923ff0b04570
Benjamin Cabé <benjamin@zephyrproject.org>
2023-03-17 14:10:04 +0100

    misc improvements
    Signed-off-by: Benjamin Cabé <benjamin@zephyrproject.org>

--- built/main.js
+++ built/main.js
@@ -17,66 +17,113 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const simple_git_1 = require("simple-git");
 const moment_1 = __importDefault(require("moment"));
 const fs_1 = __importDefault(require("fs"));
+const promises_1 = require("node:fs/promises");
 var exec = require('child-process-promise').exec;
 const DATES = [];
-let m = (0, moment_1.default)("2019-01-31");
+//let m = moment("2019-01-31");
+let m = (0, moment_1.default)().subtract(2, 'month').startOf('month');
 const beginningOfCurrentMonth = (0, moment_1.default)().startOf('month');
 while (m.isBefore(beginningOfCurrentMonth)) {
     DATES.push(m.format('YYYY-MM-DD'));
     m.add(1, 'month').endOf('month');
 }
-let git = (0, simple_git_1.simpleGit)({
-    progress({ method, stage, progress }) {
-        console.log(`git.${method} ${stage} stage ${progress}% complete`);
-    }
-});
-function countDrivers() {
+function countFoldersInSubFolder(repo, subfolder) {
     return __awaiter(this, void 0, void 0, function* () {
-        return exec('find /tmp/repos/zephyr/dts/bindings/sensor -type f | wc -l').then((res) => { return res.stdout.trim(); });
+        let workingDir = yield repo.revparse('--show-toplevel');
+        return exec(`find ${workingDir}${subfolder} -type d -maxdepth 1 | wc -l`).then((res) => { return parseInt(res.stdout.trim()); });
     });
 }
-function countSamples() {
+function countFileByNameInFolder(repo, subfolder, fileName) {
     return __awaiter(this, void 0, void 0, function* () {
-        return exec('find /tmp/repos/zephyr/samples -type f | grep sample.yaml | wc -l').then((res) => { return res.stdout.trim(); });
+        let workingDir = yield repo.revparse('--show-toplevel');
+        return exec(`find ${workingDir}${subfolder} -name ${fileName} | wc -l`).then((res) => { return parseInt(res.stdout.trim()); });
     });
 }
-function countBoards() {
-    return __awaiter(this, void 0, void 0, function* () {
-        return exec('find /tmp/repos/zephyr/boards -type f | grep /board.cmake | wc -l').then((res) => { return res.stdout.trim(); });
+function getCountFoldersInSubFolderFn(subfolder) {
+    return (repo) => __awaiter(this, void 0, void 0, function* () {
+        return countFoldersInSubFolder(repo, subfolder);
     });
 }
-function cloc() {
-    return __awaiter(this, void 0, void 0, function* () {
-        return exec('cloc /tmp/repos/zephyr --json --quiet').then((res) => { return res.stdout; });
+function getCountFileByNameInFolderFn(subfolder, fileName) {
+    return (repo) => __awaiter(this, void 0, void 0, function* () {
+        return countFileByNameInFolder(repo, subfolder, fileName);
     });
 }
-let analyticsSnippets = [
-    { name: 'drivers', fn: countDrivers },
-    { name: 'samples', fn: countSamples },
-    { name: 'boards', fn: countBoards },
-    { name: 'cloc', fn: cloc }
-];
-console.log(analyticsSnippets);
-let results = {};
-function computeStats() {
+function countZephyrDrivers(repo) {
     return __awaiter(this, void 0, void 0, function* () {
-        //cloneRepo('zephyr', 'https://github.com/zephyrproject-rtos/zephyr', '/tmp/repos/zephyr')
-        const zephyrRepo = git.clone('/tmp/zephyr-bare', '/tmp/repos/zephyr')
-            .cwd({ path: '/tmp/repos/zephyr' });
-        analyticsSnippets.forEach((snippet) => {
+        let workingDir = yield repo.revparse('--show-toplevel');
+        return exec(`find ${workingDir}/dts/bindings/sensor -type f | wc -l`).then((res) => { return parseInt(res.stdout.trim()); });
+    });
+}
+function countZephyrSamples(repo) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let workingDir = yield repo.revparse('--show-toplevel');
+        return exec(`find ${workingDir}/samples -type f | grep sample.yaml | wc -l`).then((res) => { return parseInt(res.stdout.trim()); });
+    });
+}
+function countZephyrBoards(repo) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let workingDir = yield repo.revparse('--show-toplevel');
+        return exec(`find ${workingDir}/boards -type f | grep /board.cmake | wc -l`).then((res) => { return parseInt(res.stdout.trim()); });
+    });
+}
+function cloc(repo) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let workingDir = yield repo.revparse('--show-toplevel');
+        return exec(`cloc ${workingDir} --json --quiet`).then((res) => { return parseInt(res.stdout.trim()); });
+    });
+}
+function numberOfCommits(repo) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return repo.raw(['rev-list', 'HEAD', '--count', '--first-parent']).then((x) => { return parseInt(x); });
+    });
+}
+function computeStats(repoName, repoURL, branchName = 'main', snippets) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let results = {};
+        let repoPath = './repos/' + repoName;
+        let repo;
+        let git = (0, simple_git_1.simpleGit)({
+            progress({ method, stage, progress }) {
+                //   console.log(`git.${method} ${stage} stage ${progress}% complete`);
+            }
+        });
+        try {
+            yield (0, promises_1.access)(repoPath, promises_1.constants.R_OK | promises_1.constants.W_OK);
+            console.log(`Repo ${repoURL} exists. Fetching updates...`);
+            repo = git.cwd(repoPath);
+            repo.fetch('origin');
+        }
+        catch (_a) {
+            console.log(`Repo ${repoURL} does not exist. Cloning...`);
+            repo = git.clone(repoURL, repoPath).cwd({ path: repoPath });
+        }
+        // const zephyrRepo: SimpleGit = git.clone('/tmp/zephyr-bare', '/tmp/repos/zephyr')
+        //     .cwd({ path: '/tmp/repos/zephyr' })
+        snippets.forEach((snippet) => {
             results[snippet.name] = [];
         });
+        let promises;
         for (const date of DATES) {
-            console.log(date);
-            let rev = yield zephyrRepo.raw(['rev-list', 'main', '-n', '1', '--first-parent', '--before=' + date]);
-            let sha1 = yield zephyrRepo.checkout([rev.trim()]).revparse(['HEAD']);
-            console.log('Repo now at ' + sha1);
-            const promises = analyticsSnippets.map((snippet) => snippet.fn());
-            console.log(promises);
+            console.log(`Checking out ${date} for repo ${repoName}...`);
+            try {
+                let rev = yield repo.raw(['rev-list', branchName, '-n', '1', '--first-parent', '--before=' + date]);
+                let sha1 = yield repo.checkout([rev.trim(), '-f']).revparse(['HEAD']);
+                //console.log('Repo now at ' + sha1);
+                promises = snippets.map((snippet) => snippet.fn(repo));
+            }
+            catch (e) {
+                console.log(`** ${date} -- ${repoName}. Skipping... **`);
+                let a = function () {
+                    return __awaiter(this, void 0, void 0, function* () { return null; });
+                };
+                promises = snippets.map((snippet) => a());
+            }
+            //console.log(promises);
             // Execute all the hooks in parallel
             let res = yield Promise.all(promises);
             res.forEach((r, i) => {
-                results[analyticsSnippets[i].name].push({ date: new Date(date), result: res[i] });
+                results[snippets[i].name].push({ date: new Date(date), result: res[i] });
             });
             // stats.set(new Date(date),
             //     new Map([
@@ -86,6 +133,7 @@ function computeStats() {
             //         ['cloc', JSON.parse(res[3])]
             //     ]));
         }
+        return results;
     });
 }
 function replacer(_key, value) {
@@ -99,13 +147,91 @@ function replacer(_key, value) {
         return value;
     }
 }
-computeStats().then(() => {
-    console.log('Done!');
-    // save stats as json file
-    Object.entries(results).forEach(([key, value]) => {
-        fs_1.default.writeFileSync(`${key}.json`, JSON.stringify(value));
+let projects = [
+    {
+        name: 'zephyr',
+        url: 'https://github.com/zephyrproject-rtos/zephyr',
+        branch: 'main',
+        snippets: [
+            { name: 'drivers', fn: countZephyrDrivers },
+            { name: 'samples', fn: countZephyrSamples },
+            { name: 'boards', fn: countZephyrBoards },
+            //    { name: 'cloc', fn: cloc },
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+    {
+        name: 'freertos',
+        url: 'https://github.com/FreeRTOS/FreeRTOS',
+        branch: 'main',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+    {
+        name: 'nuttx',
+        url: 'https://github.com/apache/nuttx',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits },
+            { name: 'boards', fn: getCountFileByNameInFolderFn("/boards", "Kconfig") },
+        ]
+    },
+    {
+        name: 'riot',
+        url: 'https://github.com/RIOT-OS/RIOT',
+        branch: 'master',
+        snippets: [
+            { name: 'boards', fn: getCountFoldersInSubFolderFn("/boards") },
+            { name: 'drivers', fn: getCountFoldersInSubFolderFn("/drivers") },
+            { name: 'samples', fn: getCountFoldersInSubFolderFn("/examples") },
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+    {
+        name: 'threadx',
+        url: 'https://github.com/azure-rtos/threadx',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+    {
+        name: 'mynewt',
+        url: 'https://github.com/apache/mynewt-core',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits },
+            { name: 'samples', fn: getCountFoldersInSubFolderFn("/apps") },
+        ]
+    },
+    {
+        name: 'rt-thread',
+        url: 'https://github.com/RT-Thread/rt-thread',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits },
+            { name: 'boards', fn: getCountFileByNameInFolderFn("/bsp", "board.c") },
+        ]
+    },
+];
+for (let project of projects) {
+    computeStats(project.name, project.url, project.branch, project.snippets).then((results) => {
+        console.log(`Stats for ${project.name} computed.`);
+        // save stats as json file
+        Object.entries(results).forEach(([key, value]) => {
+            fs_1.default.writeFileSync(`stats/${project.name}-${key}.json`, JSON.stringify(value));
+        });
+        console.log(`Creating CSV file for ${project.name}...`);
+        fs_1.default.writeFileSync(`stats/${project.name}.csv`, `date,${project.snippets.map(s => s.name).join(',')}\n`);
+        let idx = 0;
+        for (const date of DATES) {
+            console.log(`Processing ${date}...`);
+            fs_1.default.appendFileSync(`stats/${project.name}.csv`, `${date},${project.snippets.map(s => results[s.name][idx].result).join(',')}\n`);
+            idx++;
+        }
+    }).catch((err) => {
+        console.error(err);
     });
-}).catch((err) => {
-    console.error(err);
-});
+}
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,31 @@
 import { simpleGit, SimpleGit, ResetMode } from 'simple-git';
 import moment from 'moment';
 import fs from 'fs';
+import { access, constants } from 'node:fs/promises';
+import { worker } from 'cluster';
+
 var exec = require('child-process-promise').exec;
 
 const DATES: string[] = [];
 
-let m = moment("2019-01-31");
+
+interface IAnalyticsSnippet {
+    name: string;
+    fn(repo: SimpleGit): Promise<string | Number | null>;
+}
+
+interface IResult {
+    date: Date;
+    result: string | Number | null
+}
+
+interface IResults {
+    [key: string]: IResult[]
+}
+
+
+//let m = moment("2019-01-31");
+let m = moment().subtract(2, 'month').startOf('month');
 const beginningOfCurrentMonth = moment().startOf('month');
 
 while (m.isBefore(beginningOfCurrentMonth)) {
@@ -15,78 +35,105 @@ while (m.isBefore(beginningOfCurrentMonth)) {
     m.add(1, 'month').endOf('month');
 }
 
-let git = simpleGit({
-    progress({ method, stage, progress }) {
-        console.log(`git.${method} ${stage} stage ${progress}% complete`);
+async function countFoldersInSubFolder(repo: SimpleGit, subfolder: string): Promise<string> {
+    let workingDir = await repo.revparse('--show-toplevel');
+    return exec(`find ${workingDir}${subfolder} -type d -maxdepth 1 | wc -l`).then((res: any) => { return parseInt(res.stdout.trim()) });
+}
+
+async function countFileByNameInFolder(repo: SimpleGit, subfolder: string, fileName: string): Promise<string> {
+    let workingDir = await repo.revparse('--show-toplevel');
+    return exec(`find ${workingDir}${subfolder} -name ${fileName} | wc -l`).then((res: any) => { return parseInt(res.stdout.trim()) });
+}
+
+function getCountFoldersInSubFolderFn(subfolder: string) {
+    return async (repo: SimpleGit) => {
+        return countFoldersInSubFolder(repo, subfolder);
     }
-});
-
-
-async function countDrivers(): Promise<string> {
-    return exec('find /tmp/repos/zephyr/dts/bindings/sensor -type f | wc -l').then((res: any) => { return res.stdout.trim() });
 }
 
-async function countSamples(): Promise<string> {
-    return exec('find /tmp/repos/zephyr/samples -type f | grep sample.yaml | wc -l').then((res: any) => { return res.stdout.trim() });
+function getCountFileByNameInFolderFn(subfolder: string, fileName: string) {
+    return async (repo: SimpleGit) => {
+        return countFileByNameInFolder(repo, subfolder, fileName);
+    }
 }
 
-async function countBoards(): Promise<string> {
-    return exec('find /tmp/repos/zephyr/boards -type f | grep /board.cmake | wc -l').then((res: any) => { return res.stdout.trim() });
+async function countZephyrDrivers(repo: SimpleGit): Promise<Number> {
+    let workingDir = await repo.revparse('--show-toplevel');
+    return exec(`find ${workingDir}/dts/bindings/sensor -type f | wc -l`).then((res: any) => { return parseInt(res.stdout.trim()) });
 }
 
-async function cloc(): Promise<string> {
-    return exec('cloc /tmp/repos/zephyr --json --quiet').then((res: any) => { return res.stdout });
+async function countZephyrSamples(repo: SimpleGit): Promise<Number> {
+    let workingDir = await repo.revparse('--show-toplevel');
+    return exec(`find ${workingDir}/samples -type f | grep sample.yaml | wc -l`).then((res: any) => { return parseInt(res.stdout.trim()) });
+}
+
+async function countZephyrBoards(repo: SimpleGit): Promise<Number> {
+    let workingDir = await repo.revparse('--show-toplevel');
+    return exec(`find ${workingDir}/boards -type f | grep /board.cmake | wc -l`).then((res: any) => { return parseInt(res.stdout.trim()) });
+}
+
+async function cloc(repo: SimpleGit): Promise<Number> {
+    let workingDir = await repo.revparse('--show-toplevel');
+    return exec(`cloc ${workingDir} --json --quiet`).then((res: any) => { return parseInt(res.stdout.trim()) });
+}
+
+async function numberOfCommits(repo: SimpleGit): Promise<Number> {
+    return repo.raw(['rev-list', 'HEAD', '--count', '--first-parent']).then((x) => { return parseInt(x) });
 }
 
 
-interface IAnalyticsSnippet {
-    name: string;
-    fn(): Promise<string>;
-}
-
-let analyticsSnippets: IAnalyticsSnippet[] = [
-    { name: 'drivers', fn: countDrivers },
-    { name: 'samples', fn: countSamples },
-    { name: 'boards', fn: countBoards },
-    { name: 'cloc', fn: cloc }
-];
-
-console.log(analyticsSnippets);
-
-interface IResult {
-    date: Date;
-    result: string
-}
-
-interface IResults {
-    [key: string]: IResult[]
-}
-
-let results: IResults = {}
 
 
-async function computeStats() {
-    //cloneRepo('zephyr', 'https://github.com/zephyrproject-rtos/zephyr', '/tmp/repos/zephyr')
-    const zephyrRepo: SimpleGit = git.clone('/tmp/zephyr-bare', '/tmp/repos/zephyr')
-        .cwd({ path: '/tmp/repos/zephyr' })
+async function computeStats(repoName: string, repoURL: string, branchName: string = 'main', snippets: IAnalyticsSnippet[]) {
+    let results: IResults = {}
 
-    analyticsSnippets.forEach((snippet: IAnalyticsSnippet) => {
+    let repoPath = './repos/' + repoName;
+    let repo: SimpleGit;
+
+    let git = simpleGit({
+        progress({ method, stage, progress }) {
+            //   console.log(`git.${method} ${stage} stage ${progress}% complete`);
+        }
+    });
+
+    try {
+        await access(repoPath, constants.R_OK | constants.W_OK);
+        console.log(`Repo ${repoURL} exists. Fetching updates...`)
+        repo = git.cwd(repoPath);
+        repo.fetch('origin');
+    } catch {
+        console.log(`Repo ${repoURL} does not exist. Cloning...`)
+        repo = git.clone(repoURL, repoPath).cwd({ path: repoPath });
+    }
+
+
+    // const zephyrRepo: SimpleGit = git.clone('/tmp/zephyr-bare', '/tmp/repos/zephyr')
+    //     .cwd({ path: '/tmp/repos/zephyr' })
+
+    snippets.forEach((snippet: IAnalyticsSnippet) => {
         results[snippet.name] = [];
     });
 
+    let promises: Promise<string | Number | null>[];
     for (const date of DATES) {
-        console.log(date)
-        let rev: string = await zephyrRepo.raw(['rev-list', 'main', '-n', '1', '--first-parent', '--before=' + date])
-        let sha1: string = await zephyrRepo.checkout([rev.trim()]).revparse(['HEAD']);
-        console.log('Repo now at ' + sha1);
-
-        const promises = analyticsSnippets.map((snippet: IAnalyticsSnippet) => snippet.fn());
-        console.log(promises);
+        console.log(`Checking out ${date} for repo ${repoName}...`)
+        try {
+            let rev: string = await repo.raw(['rev-list', branchName, '-n', '1', '--first-parent', '--before=' + date])
+            let sha1: string = await repo.checkout([rev.trim(), '-f']).revparse(['HEAD']);
+            //console.log('Repo now at ' + sha1);
+            promises = snippets.map((snippet: IAnalyticsSnippet) => snippet.fn(repo));
+        }
+        catch (e) {
+            console.log(`** ${date} -- ${repoName}. Skipping... **`)
+            let a = async function () { return null }
+            promises = snippets.map((snippet: IAnalyticsSnippet) => a());
+        }
+        //console.log(promises);
 
         // Execute all the hooks in parallel
         let res = await Promise.all(promises)
         res.forEach((r, i) => {
-            results[analyticsSnippets[i].name].push({ date: new Date(date), result: res[i] });
+            results[snippets[i].name].push({ date: new Date(date), result: res[i] });
         })
 
         // stats.set(new Date(date),
@@ -98,6 +145,7 @@ async function computeStats() {
         //     ]));
 
     }
+    return results;
 }
 
 function replacer(_key: any, value: any) {
@@ -111,12 +159,106 @@ function replacer(_key: any, value: any) {
     }
 }
 
-computeStats().then(() => {
-    console.log('Done!');
-    // save stats as json file
-    Object.entries(results).forEach(([key, value]) => {
-        fs.writeFileSync(`${key}.json`, JSON.stringify(value));
+let projects = [
+    {
+        name: 'zephyr',
+        url: 'https://github.com/zephyrproject-rtos/zephyr',
+        branch: 'main',
+        snippets: [
+            { name: 'drivers', fn: countZephyrDrivers },
+            { name: 'samples', fn: countZephyrSamples },
+            { name: 'boards', fn: countZephyrBoards },
+            //    { name: 'cloc', fn: cloc },
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+
+    },
+
+    {
+        name: 'freertos',
+        url: 'https://github.com/FreeRTOS/FreeRTOS',
+        branch: 'main',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+
+    {
+        name: 'nuttx',
+        url: 'https://github.com/apache/nuttx',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits },
+            { name: 'boards', fn: getCountFileByNameInFolderFn("/boards", "Kconfig") },
+        ]
+    },
+
+    {
+        name: 'riot',
+        url: 'https://github.com/RIOT-OS/RIOT',
+        branch: 'master',
+        snippets: [
+            { name: 'boards', fn: getCountFoldersInSubFolderFn("/boards") },
+            { name: 'drivers', fn: getCountFoldersInSubFolderFn("/drivers") },
+            { name: 'samples', fn: getCountFoldersInSubFolderFn("/examples") },
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+
+    {
+        name: 'threadx',
+        url: 'https://github.com/azure-rtos/threadx',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits }
+        ]
+    },
+
+    {
+        name: 'mynewt',
+        url: 'https://github.com/apache/mynewt-core',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits },
+            { name: 'samples', fn: getCountFoldersInSubFolderFn("/apps") },
+        ]
+    },
+
+    {
+        name: 'rt-thread',
+        url: 'https://github.com/RT-Thread/rt-thread',
+        branch: 'master',
+        snippets: [
+            { name: 'numberOfCommits', fn: numberOfCommits },
+            { name: 'boards', fn: getCountFileByNameInFolderFn("/bsp", "board.c") },
+
+        ]
+    },
+]
+
+
+for (let project of projects) {
+    computeStats(project.name, project.url, project.branch, project.snippets).then((results) => {
+        console.log(`Stats for ${project.name} computed.`);
+        // save stats as json file
+        Object.entries(results).forEach(([key, value]) => {
+            fs.writeFileSync(`stats/${project.name}-${key}.json`, JSON.stringify(value));
+        });
+
+        console.log(`Creating CSV file for ${project.name}...`)
+        fs.writeFileSync(`stats/${project.name}.csv`, `date,${project.snippets.map(s => s.name).join(',')}\n`);
+        let idx = 0;
+        for (const date of DATES) {
+            console.log(`Processing ${date}...`)
+            fs.appendFileSync(`stats/${project.name}.csv`, `${date},${project.snippets.map(s => results[s.name][idx].result).join(',')}\n`);
+            idx++;
+        }
+
+
+
+    }).catch((err) => {
+        console.error(err);
     });
-}).catch((err) => {
-    console.error(err);
-});
+}
+
+
